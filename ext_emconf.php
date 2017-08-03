41cd876cfe61176ad3cfa5b68669673a718d2a16
Lidia Demin <demin@webit.de>
2017-08-03 17:39:48 +0200

    [FEATURE] Add extension for additional typo3_console commands
    Refs #48156 #48157

---
+++ ext_emconf.php
@@ -0,0 +1,36 @@
+<?php
+
+/***************************************************************
+ * Extension Manager/Repository config file for ext: "sys_log_watcher_task"
+ *
+ * Manually generated 2017-07-31
+ *
+ * Manual updates:
+ * Only the data in the array - anything else is removed by next write.
+ * "version" and "dependencies" must not be touched!
+ ***************************************************************/
+
+$EM_CONF[$_EXTKEY] = array(
+    'title' => 'typo3_console_addons',
+    'description' => 'Addons to the typo3_console extension',
+    'category' => 'plugin',
+    'author' => 'Lidia Demin',
+    'author_email' => 'demin@webit.de',
+    'author_company' => 'webit! GmbH',
+    'state' => 'alpha',
+    'internal' => '',
+    'uploadfolder' => '0',
+    'createDirs' => '',
+    'clearCacheOnLoad' => 0,
+    'version' => '0.0.1',
+    'constraints' => array(
+        'depends' => array(
+            'php' => '7.0.0-7.1.99',
+            'typo3' => '7.6.99-8.7.99',
+            'cms' => '',
+            'typo3_console' => '^4.6'
+        ),
+        'conflicts' => array(),
+        'suggests' => array(),
+    ),
+);
